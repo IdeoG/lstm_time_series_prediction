c03b29b3277cd764f36a1f6081430fa12dcaee39
IdeoG <ideogaib@gmail.com>
2019-07-02 16:45:37 +0300

    docs: added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lstm-time-series-prediction',
+    version='v0.0.2',
+    packages=[''],
+    url='',
+    license='',
+    author='IdeoG',
+    author_email='',
+    description='', install_requires=['tensorboardX', 'tqdm', 'torch', 'numpy']
+)
